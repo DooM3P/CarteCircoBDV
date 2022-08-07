4a73053bf869b05eaaf6b02ff2d7e51090cf8b1b
DooM3P <ludovic.lecoq@gmail.com>
2022-08-07 23:00:28 +0200

    ajout commentaires

--- ScrapingPrefect.py
+++ ScrapingPrefect.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+# Extraction de la liste des .pdf à partir du html
 def listurl(url):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     response = requests.request("GET", url, headers=headers)
